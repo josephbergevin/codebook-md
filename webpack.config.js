441c19077d8f2a9898cc0f2f07d2998824042c06
Joe Bergevin <josephbergevin@gmail.com>
2025-02-07 19:44:16 -0700

    test(codebook): :lipstick: cleaned up the tests and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,18 @@
 
 'use strict';
 
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-//@ts-check
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
 
 /** @type WebpackConfig */
 const extensionConfig = {
   target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+  mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
   entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
@@ -45,4 +48,5 @@ const extensionConfig = {
     level: "log", // enables logging required for problem matchers
   },
 };
-module.exports = [ extensionConfig ];
+
+export default [extensionConfig];
