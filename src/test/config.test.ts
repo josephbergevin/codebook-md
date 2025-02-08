36c1b05393f597ddba1e202d89e03d85ed6eb46e
Joe Bergevin <josephbergevin@gmail.com>
2025-02-07 19:53:05 -0700

    test: :white_check_mark: converted tests to use Jest instead of Mocha

--- src/test/config.test.ts
+++ src/test/config.test.ts
@@ -1,22 +1,17 @@
-import { describe, it } from 'mocha';
-import { expect } from 'chai';
-
-// import the functions from config.ts
 import * as config from '../config';
 
-// tests for config.ts
 describe('config.ts Test Suite', () => {
   const workspacePath = '/Users/tijoe/go/src/github.com/josephbergevin/codebook-md';
   it('fullTempPath with relative folder', () => {
     const tempPath = './temp';
     const currentFile = '/Users/tijoe/go/src/github.com/josephbergevin/codebook-md/src/extension.ts';
     const message = config.fullTempPath(tempPath, currentFile, workspacePath);
-    expect(message).to.equal('/Users/tijoe/go/src/github.com/josephbergevin/codebook-md/src/temp');
+    expect(message).toBe('/Users/tijoe/go/src/github.com/josephbergevin/codebook-md/src/temp');
   });
   it('fullTempPath with empty tempPath', () => {
     const tempPath = '';
     const currentFile = '/Users/tijoe/go/src/github.com/josephbergevin/codebook-md/src/extension.ts';
     const message = config.fullTempPath(tempPath, currentFile, workspacePath);
-    expect(message).to.equal('/Users/tijoe/go/src/github.com/josephbergevin/codebook-md');
+    expect(message).toBe('/Users/tijoe/go/src/github.com/josephbergevin/codebook-md');
   });
 });
