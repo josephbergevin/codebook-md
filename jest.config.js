2cc42eeb592cb74935e62a683a39ab7e4ec4ecfe
Joe Bergevin <josephbergevin@gmail.com>
2025-03-13 12:15:16 -0600

    fix(build): migrate Jest and Webpack configurations to CommonJS format

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,13 @@
 /** @type {import('jest').Config} */
-export default {
-  preset: 'ts-jest/presets/default-esm',
+module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/src/test/**/*.test.ts'],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
   transform: {
     '^.+\\.tsx?$': [
       'ts-jest',
       {
-        useESM: true,
+        useESM: false,
       },
     ],
   },
