358035f5526bb884afdfb3101866b56acb6be4e3
Joe Bergevin <josephbergevin@gmail.com>
2025-03-13 12:46:57 -0600

    fix(eslint): enhance config for CommonJS and add rules for webpack

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,40 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+        __dirname: "readonly",
+        require: "readonly",
+        module: "readonly"
+      }
+    }
+  },
+  {
+    files: ["webpack.config.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+        __dirname: "readonly",
+        require: "readonly",
+        module: "readonly"
+      }
+    },
+    rules: {
+      "@typescript-eslint/no-var-requires": "off"
+    }
+  }
 ];
