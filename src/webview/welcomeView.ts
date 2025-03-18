d2c44148ba97fff2d2df2e9e56c5865a2994b903
Joe Bergevin <josephbergevin@gmail.com>
2025-03-18 01:10:21 -0600

    feat: welcome view - use HTML template for improved maintainability

--- src/webview/welcomeView.ts
+++ src/webview/welcomeView.ts
@@ -6,6 +6,7 @@ import {
   Disposable
 } from 'vscode';
 import * as path from 'path';
+import * as fs from 'fs';
 
 /**
  * Provider for the Welcome webview view.
@@ -64,157 +65,13 @@ export class WelcomeViewProvider implements WebviewViewProvider, Disposable {
     const logoPath = path.join(this._extensionContext.extensionPath, 'extension/src/img/logo_3_800x800.png');
     const logoUri = this._view?.webview.asWebviewUri(Uri.file(logoPath));
 
-    return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Welcome to Codebook MD</title>
-      <style>
-        body {
-          font-family: var(--vscode-font-family);
-          font-size: var(--vscode-font-size);
-          color: var(--vscode-foreground);
-          padding: 0;
-          margin: 0;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-        }
-        .container {
-          padding: 10px;
-          width: 100%;
-          box-sizing: border-box;
-        }
-        .logo-container {
-          padding-bottom: 10px;
-          max-width: 100%;
-          text-align: center;
-        }
-        .logo {
-          max-width: 128px;
-          height: auto;
-        }
-        h1, h4, h5 {
-          text-align: center;
-          margin: 0.8em 0 0.5em 0;
-        }
-        h1 {
-          font-size: 1.5em;
-        }
-        h4 {
-          font-size: 1.2em;
-          margin-top: 1.5em;
-        }
-        h5 {
-          font-size: 1.1em;
-          text-align: left;
-        }
-        p {
-          margin: 0.5em 0;
-          text-align: left;
-          line-height: 1.5;
-        }
-        ul {
-          text-align: left;
-          padding-left: 20px;
-        }
-        li {
-          margin-bottom: 0.3em;
-        }
-        ol {
-          text-align: left;
-          padding-left: 20px;
-        }
-        .feature-list li {
-          margin-bottom: 0.5em;
-        }
-        .instructions {
-          margin-top: 1em;
-          text-align: left;
-        }
-        .feature-highlight {
-          font-weight: bold;
-        }
-        .code-example {
-          font-family: var(--vscode-editor-font-family);
-          background-color: var(--vscode-editor-background);
-          padding: 2px 4px;
-          border-radius: 3px;
-        }
-        hr {
-          border: none;
-          border-top: 1px solid var(--vscode-panel-border);
-          margin: 1.5em 0;
-        }
-      </style>
-    </head>
-    <body>
-      <div class="container">
-        <div class="logo-container">
-          <img class="logo" src="${logoUri}" alt="Codebook MD Logo" />
-        </div>
-        <h1>Codebook MD</h1>
-        <p>
-          Bring your markdown to life! Execute code blocks and keep your notebooks organized.
-        </p>
-        
-        <hr>
-        
-        <h4>Organizing Markdown Files with Virtual Folders</h4>
-        
-        <h5>Features</h5>
-        <ul class="feature-list">
-          <li><span class="feature-highlight">Organize <code class="code-example">.md</code> files</span> into virtual folders</li>
-          <li><span class="feature-highlight">Create custom folder hierarchies</span> for easy navigation</li>
-          <li><span class="feature-highlight">Add frequently used notebooks</span> to specific folders</li>
-          <li><span class="feature-highlight">Rename files and folders</span> with custom display names</li>
-          <li><span class="feature-highlight">Access important documents</span> with one click</li>
-        </ul>
-        
-        <p>
-          The <span class="feature-highlight">Tree View</span> feature allows you to create virtual folders to organize your markdown files.
-          You can add files to these folders, making it easier to navigate and manage your workspace.
-        </p>
-        
-        <div class="instructions">
-          <h5>How to Add Virtual Folders</h5>
-          <ol>
-            <li>Click on the <span class="feature-highlight">New Folder</span> icon above the <span class="feature-highlight">Tree View</span>.</li>
-            <li>Enter the display name of the folder.
-              <ul>
-                <li>Example: <code class="code-example">"Animals"</code></li>
-              </ul>
-            </li>
-            <li>The virtual folder will be added to the top level of the <span class="feature-highlight">Tree View</span>.</li>
-          </ol>
-          
-          <h5>How to Add Sub-Folders</h5>
-          <ol>
-            <li>Right-click on the folder you want to add a sub-folder to.</li>
-            <li>Select <span class="feature-highlight">"Add Sub-Folder"</span> from the context menu.</li>
-            <li>Enter the display name of the sub-folder.
-              <ul>
-                <li>Example: <code class="code-example">"Dogs"</code></li>
-              </ul>
-            </li>
-            <li>The sub-folder will be added to the selected folder in the <span class="feature-highlight">Tree View</span>.</li>
-          </ol>
-          
-          <h5>How to Add Files to Folders</h5>
-          <ol>
-            <li>Right-click on the folder you want to add a file to.</li>
-            <li>Select <span class="feature-highlight">"Add File"</span> from the context menu.</li>
-            <li>Enter the display name of the file.
-              <ul>
-                <li>Example: <code class="code-example">"Dog Breeds"</code></li>
-              </ul>
-            </li>
-            <li>The file will be added to the selected folder in the <span class="feature-highlight">Tree View</span>.</li>
-          </ol>
-        </div>
-      </div>
-    </body>
-    </html>`;
+    // Read the HTML template
+    const templatePath = path.join(this._extensionContext.extensionPath, 'src/webview/templates/welcome.html');
+    let htmlContent = fs.readFileSync(templatePath, 'utf8');
+
+    // Replace placeholder with actual logo URI
+    htmlContent = htmlContent.replace('{{logoUri}}', logoUri?.toString() || '');
+
+    return htmlContent;
   }
 }
